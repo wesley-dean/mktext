75849833fff18508f272fd26fc9a330f2d7d6744
Wes Dean <wesley-dean@users.noreply.github.com>
2026-08-17 19:14:03 -0400

    chore: remove generated reference documentation

--- doc/reference/search/variables_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['_5f_5fmktext_5fbuild_5fcommit_0',['__mktext_build_commit',['../mktext_8bash.html#a0e6fd5ea141e380cfa117c0934d0739e',1,'mktext.bash']]],
-  ['_5f_5fmktext_5fbuild_5fdate_1',['__mktext_build_date',['../mktext_8bash.html#a6d1b0fcf66b383749a866b04806fc25e',1,'mktext.bash']]],
-  ['_5f_5fmktext_5fversion_2',['__mktext_version',['../mktext_8bash.html#a0c188336429541345d8364ff37655955',1,'mktext.bash']]]
-];
